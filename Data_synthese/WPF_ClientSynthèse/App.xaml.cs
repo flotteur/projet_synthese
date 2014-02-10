2fbbf42dd7aa98276a69413ed22b8ce37cbb51de
Denis Duplain <d.duplain@hotmail.com>
2014-02-09 22:17:27 -0500

    Summary
    Description

---
+++ Data_synthese/WPF_ClientSynthèse/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+
+namespace WPF_ClientSynthèse
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application, IDisposable
+    {
+      
+
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing) {
+            //if (disposing)
+            //    if (Proxy != null) {
+            //        Proxy. .Dispose();
+            //        Proxy = null;
+            //    }
+        }
+
+        ~App() {
+            Dispose(false);
+        }
+    }
+}
