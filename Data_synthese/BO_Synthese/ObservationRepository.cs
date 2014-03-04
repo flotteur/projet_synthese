e04626a023f3062251246f6f1f9bfebb217cc9d8
Flotteur <flotteur@gmail.com>
2014-03-03 21:49:48 -0500

    Still reimplementing some things....

---
+++ Data_synthese/BO_Synthese/ObservationRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BO_Synthese
+{
+    class ObservationRepository
+    {
+        #region propriétés
+
+        #endregion
+
+        #region public
+
+        #endregion
+
+        #region private
+
+        #endregion
+    }
+}
