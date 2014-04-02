dbdf8515afc85faee65e04b3c9ea35ffa7ec0e9a
Flotteur <flotteur@gmail.com>
2014-04-02 19:35:53 -0400

    ??? WTF GIT

--- Data_synthese/WCF_Synthese/IServiceWCF_Synthese.cs
+++ Data_synthese/WCF_Synthese/IServiceWCF_Synthese.cs
@@ -174,7 +174,7 @@ namespace WCF_Synthese
         #endregion
 
         #region " Alerte "
-
+        /*
         [OperationContract]
         [WebGet(
         BodyStyle = WebMessageBodyStyle.Wrapped,
@@ -182,14 +182,15 @@ namespace WCF_Synthese
         ResponseFormat = WebMessageFormat.Json,
         UriTemplate = "Alerte")]
         List<AlerteWCF> ObtenirAlerte(string ID);
-
+        */
+        /*
         [OperationContract]
         [WebInvoke(Method = "GET",
         BodyStyle = WebMessageBodyStyle.Wrapped,
         RequestFormat = WebMessageFormat.Json,
         ResponseFormat = WebMessageFormat.Json,
         UriTemplate = "Alerte")]
-        List<AlerteWCF> ObtenirAlertes();
+        List<AlerteWCF> ObtenirAlertes();*/
 
         [OperationContract]
         [WebInvoke(Method = "DELETE",
